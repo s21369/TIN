efc5ebe27cebca18554c28579f997434220c87fd
Bohdan Kordon <s21369@pjwstk.edu.pl>
2021-10-31 19:53:48 +0100

    fix task E

--- Lab-04/4e.js
+++ Lab-04/4e.js
@@ -1,7 +1,8 @@
+const regex = /[^a-zA-Z\s]/g;
 let string = "Lorem ninja ipsum dolor sit amet, consectetuer adipiscing elit";
 
 function longestWord(str) {
-    let words = str.split(" ");
+    let words = str.replaceAll(regex, "").split(" ");
     let wordsLength = words.map(word => word.length);
     return words[wordsLength.indexOf(Math.max(...wordsLength))];
 }
